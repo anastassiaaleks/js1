08a979d9116a5ca960b0178f095cf21818dee6f5
Anastasia Aleksandrova <aleksandrovaanastasia2302@gmail.com>
2021-01-29 15:42:32 +0300

    First commit

---
+++ scripts.js
@@ -0,0 +1,135 @@
+//Задание 1
+
+let str='Привет',
+    sum=123,
+    num=15.8,
+    flag=true,
+    txt='true';
+
+console.log(str, typeof str);
+console.log(sum, typeof sum);
+console.log(num, typeof num);
+console.log(flag, typeof flag);
+console.log(txt, typeof txt);
+
+//Задание 2
+
+let a1=5%3,
+    a2=3%5,
+    a3=5+'3',
+    a4='5'-3,
+    a5=75+'кг',
+    a6=785*653,
+    a7=100/25,
+    a8=0*0,
+    a9=0/2,
+    a10=89/0,
+    a11=98+2,
+    a12=5-98,
+    a13=(8+56*4)/5,
+    a14=(9-12)*7/(5+2),
+    a15=+"123",
+    a16=1||0,
+    a17=false||true,
+    a18=true>0;
+console.log(a1, typeof a1);
+console.log(a2, typeof a2);
+console.log(a3, typeof a3);
+console.log(a4, typeof a4);
+console.log(a5, typeof a5);
+console.log(a6, typeof a6);
+console.log(a7, typeof a7);
+console.log(a8, typeof a8);
+console.log(a9, typeof a9);
+console.log(a10, typeof a10);
+console.log(a11, typeof a11);
+console.log(a12, typeof a12);
+console.log(a13, typeof a13);
+console.log(a14, typeof a14);
+console.log(a15, typeof a15);
+console.log(a16, typeof a16);
+console.log(a17, typeof a17);
+console.log(a18, typeof a18);
+
+//Задание 3
+
+let width=10,
+    height=23;
+SPryam=width*height;
+console.log(SPryam+' см');
+
+//Задание 4
+
+let VCilindra=3.1415*10*a7**2;
+console.log(VCilindra+' м');
+
+//Задание 5
+
+let r=5;
+SKruga=3.1415*r**2;
+console.log(SKruga+' см');
+
+//Задание 6
+
+let a=5,
+    b=7,
+    h=10;
+STrap=(a+b)/2*h;
+console.log(STrap+' см');
+
+//Задание 7
+
+let S=2,
+    years=5,
+    p=0.1;
+Pereplata=S*years*p;
+console.log(Pereplata+' млн');
+
+//Задание 8
+
+let q=8,
+    d=3,
+    x=d-q/2+8;
+console.log(x);
+x=(q-15*d)/(d-6);
+console.log(x);
+x=23780/(3+q+d);
+console.log(x);
+
+//Задание 9
+
+let poem1='Бывало, спит у ног собака,',
+    poem2='костер занявшийся гудит,',
+    poem3='и женщина из полумрака',
+    poem4='глазами зыбкими глядит.',
+    poem5='Потом под пихтою приляжет',
+    poem6='на куртку рыжую мою',
+    poem7='и мне, задумчивая, скажет:',
+    poem8='"А ну-ка, спой!.."- и я пою.';
+
+console.log(poem1);
+console.log(poem2);
+console.log(poem3);
+console.log(poem4);
+console.log('');
+console.log('');
+console.log(poem5);
+console.log(poem6);
+console.log(poem7);
+console.log('');
+console.log('');
+console.log(poem8);
+
+
+//Задание 10
+
+let str1='индо земля зашаталась под ногами-и вырос,',
+    str2='и заревел он голосом диким...',
+    str3='блеснула молния и ударил гром,',
+    str4='а так какое-то чудище, страшное и мохнатое,',
+    str5='как будто из-под земли, перед купцом:',
+    str6='Он подошёл и сорвал аленький цветочек.',
+    str7='зверь не зверь, человек не человек,',
+    str8='В ту же минуту, безо всяких туч,';
+
+console.log(str6, str8, str3, str1, str5, str7, str4, str2)
